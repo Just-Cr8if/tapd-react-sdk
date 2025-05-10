3934bcee20b7a0f1d56dcfc6756dd0aa1b8961bc
jimmyfl0 <jflorence0@gmail.com>
2025-05-10 17:51:07 -0400

    TapdStorage and ToastPromt added

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 export { default as CustomerPrompt } from './components/CustomerPrompt';
 export { default as ToastPrompt } from './components/ToastPrompt';
-export { default as TapdStorage, markPromptSeen, hasSeenPrompt, clearPromptSeen, resetPromptAfter, saveCustomerId, getCustomerId, clearCustomerId, submitCustomerData, submitCustomerSkip, TAPD_API_URL, } from './utils/tapdStorage';
+export { default as TapdStorage } from './utils/tapdStorage';
+export * from './utils/tapdStorage';
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
 export { default as CustomerPrompt } from './components/CustomerPrompt';
 export { default as ToastPrompt } from './components/ToastPrompt';
 export type { CustomerData, SubmitResponse } from './utils/tapdStorage';
-
-export {
-  default as TapdStorage,
-  markPromptSeen,
-  hasSeenPrompt,
-  clearPromptSeen,
-  resetPromptAfter,
-  saveCustomerId,
-  getCustomerId,
-  clearCustomerId,
-  submitCustomerData,
-  submitCustomerSkip,
-  TAPD_API_URL,
-} from './utils/tapdStorage';
+export { default as TapdStorage } from './utils/tapdStorage';
+export * from './utils/tapdStorage';
